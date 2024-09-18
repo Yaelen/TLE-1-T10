268887a2f8d18e9d943615d555d1b5e8cddf721e
yimingeling <yiming@live.nl>
2024-09-18 10:45:01 +0200

    auth page aangemaakt

---
+++ includes/authentication.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['first_name'])) {
+    header("Location: login.php");
+    exit();
+}
+
+?>
---
+++ seedSeed.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<main>
+    <h1>de seeds</h1>
+
+
+        <?php foreach ($products as $product) { ?>
+    <section>
+            <h1> asddsa </h1>
+        <p>adsads</p>
+    </section>
+            > <?php } ?>
+
+</main>
+
+</body>
+</html>
