338ea00105cecc9a8aca2a6113f79ed8ab0d0978
meya <milanela.milanova@gmail.com>
2024-09-23 10:17:15 +0200

    park page

--- parkinfo.php
+++ parkinfo.php
@@ -1,13 +1,69 @@
 <!doctype html>
 <html lang="en">
-<meta charset="UTF-8">
-<meta name="viewport"
-      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>GAIA Park</title>
-<link rel="stylesheet" href="/css/style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GAIA Park</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+<nav>
+    <a></a>
+</nav>
+<section class="park-container">
+    <div class="park-column" >
+        <div>
+            <h1>MOUNT DENALI</h1>
+            <h2>ALASKA AMERICA</h2>
+        </div>
+        <div>
 
+        </div>
+    </div>
+    <div class="park-column" >
+        <div>
+            <h1>MANGROVE FOREST</h1>
+            <h2>BANGLADESH INDIA</h2>
+        </div>
+        <div>
+
+        </div>
+    </div>
+    <div class="park-column" >
+        <div>
+            <h1>PLITIVCE LAKE</h1>
+            <h2>QUEENSLAND CROATIA</h2>
+        </div>
+        <div>
+
+        </div>
+
+    </div>
+    <div class="park-column" >
+        <div>
+            <h1>AMAZON RAINFOREST</h1>
+            <h2>SOUTH AMERICA</h2>
+        </div>
+        <div>
+
+        </div>
+
+    </div>
+    <div class="park-column" >
+        <div>
+            <h1>MOUNT RORAIMA</h1>
+            <h2>SOUTH AMERICA</h2>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</section>
+
+<footer>
+
+</footer>
 </body>
 </html>
