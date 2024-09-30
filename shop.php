a8f07d6f587dddab437f94153ff9cc378b1a91f3
yaelen <yaelensardjo@gmail.com>
2024-09-30 10:13:47 +0200

    Park-page v1.0

---
+++ shop.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Discover GAIA</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/gaiaDiscover.css">
+
+</head>
+
+<body class="frontpage-body">
+
+<nav>
+    <div class="links">
+        <a href="parkinfo.php">PARK</a>
+        <a href="seed.php">GAIASEED</a>
+        <a href="ams.php">ANIMALS</a>
+        <a href="donate.php">SUPPORT</a>
+        <a href="events.php">DISCOVER</a>
+    </div>
+    <a href="index.php" id="logo">GAIA</a>
+</nav>
+
+<header class="frontpage">
+</header>
+
+</body>
+</html>
